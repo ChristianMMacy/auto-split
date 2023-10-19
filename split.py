551e410990f88ae2d817a976cda16f335460f9c5
Christian M. Macy <christian@sparrowcompass.com>
2023-10-18 23:09:01 -0700

    Calculate duration from start/end

--- split.py
+++ split.py
@@ -1,13 +1,19 @@
 import os
 import yaml
 from ffmpeg import FFmpeg
+from datetime import time, datetime, date
 
 with open('files.yml', 'r') as raw_file:
     file_config = yaml.safe_load(raw_file)
 
+default_time = "00:00"
 
 def main():
     for file in file_config["output_files"]:
+        start = time.fromisoformat(file.get("start", default_time))
+        end = time.fromisoformat(file.get("end", default_time))
+        delta = datetime.combine(date.min, end) - datetime.combine(date.min, start)
+        duration = file.get("duration", (datetime.utcfromtimestamp(0) + delta).strftime("%H:%M:%S.%f")[:-5])
         if os.path.isfile(file["name"]):
             os.remove(file["name"])
         ffmpeg = (
@@ -16,7 +22,7 @@ def main():
             .output(
                 file["name"],
                 c="copy",
-                t=file["duration"]
+                t=duration
             )
         )
 
