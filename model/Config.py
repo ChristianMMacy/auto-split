afc8c5be7d98cddebf513c4ee957e66de9b0606e
Christian M. Macy <christian.m.macy@gmail.com>
2023-10-27 05:48:17 -0700

    Add YouTube API test/connect method

--- model/Config.py
+++ model/Config.py
@@ -1,6 +1,9 @@
 import contextlib
+from os import getenv
+
 import yaml
 from model import OutputFile, File
+from dotenv import load_dotenv
 
 
 class Config(contextlib.ExitStack):
@@ -9,8 +12,15 @@ class Config(contextlib.ExitStack):
     input_file: File
     output_files = []
 
+    refresh_token: None
+    secret_config: None
+
     def __init__(self):
         super().__init__()
+        load_dotenv('.env.local')
+        self.refresh_token = getenv('REFRESH_TOKEN')
+        self.secret_config = getenv('SECRET_CONFIG')
+
         raw_config_file = self.enter_context(open(self.file_path, 'r'))
         config_file = yaml.safe_load(raw_config_file)
 
--- tests/test_split.py
+++ tests/test_split.py
@@ -1,8 +1,6 @@
 import unittest
-import datetime
 from .context import split
 
 class SplitTest(unittest.TestCase):
-    def test_format_time(self):
-        simple_time = datetime.timedelta(0,10)
-        self.assertEqual(split.format_time(simple_time), "00:00:10.0")
+    def test_youtube(self):
+        split.hit_youtube()
