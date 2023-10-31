51ee3f2fc2eda0f2478b4d840a7996d961c6d6eb
Christian M. Macy <christian@sparrowcompass.com>
2023-10-30 17:13:28 -0700

    Upload to YT works
    This is an interim fix bypassing the refresh_token

--- split.py
+++ split.py
@@ -1,8 +1,12 @@
 import json
 import os
+
 from ffmpeg import FFmpeg
+from pyyoutube import Client, AccessToken, Video, VideoSnippet, Thumbnails
+from pyyoutube.media import Media
+from pyyoutube.models.common import Thumbnail
+
 from model.Config import Config
-from pyyoutube import Client, AccessToken
 
 
 def main():
@@ -33,28 +37,42 @@ def main():
 
 def hit_youtube():
     config = Config()
-    scopes = ["https://www.googleapis.com/auth/youtube.upload"]
+    scopes = ["https://www.googleapis.com/auth/youtube",
+              "https://www.googleapis.com/auth/youtube.force-ssl",
+              "https://www.googleapis.com/auth/userinfo.profile", ]
     with (open(config.secret_config) as raw_secret_config):
         secret_config = json.loads(raw_secret_config.read())
         client_id = secret_config.get("installed").get("client_id")
         client_secret = secret_config.get("installed").get("client_secret")
 
         client = Client(client_id=client_id, client_secret=client_secret)
-        auth_url_response = client.get_authorize_url(scope=scopes)
 
         if config.refresh_token is None:
+            authorize_url, state = client.get_authorize_url(scope=scopes, access_type="offline")
+            print('Click the URL to authenticate and copy the redirected URL:\n', authorize_url)
+            url = input('Enter the redirected URL\n')
             # Debug and inspect this response to get the refresh token
             # Visit the [url,] from auth_url_response, sign in, and get the "code" query param from the response
-            # Run again to get the access_token_response and add the refresh token to environment (e.g. via .env.example.local)
+            # Run again to get the access_token_response and add the refresh token to environment (e.g. via .env)
             access_token_response = client.generate_access_token(
-                code="Follow Steps Above to Generate This",
-                scope=scopes, return_json=True)
+                authorization_response=url,
+                scope=scopes)
+            print(f"Your token: {access_token_response}")
         else:
             access_token_response = client.refresh_access_token(config.refresh_token)
-        AccessToken(access_token=access_token_response.access_token, expires_in=access_token_response.expires_in,
-                    token_type=access_token_response.token_type)
-        print(access_token_response)
-        print(auth_url_response)
+            print(f"Your token: {access_token_response}")
+        resp = client.channels.list(mine=True)
+        print(f"Your channel id: {resp.items[0].id}")
+        body = Video(snippet=VideoSnippet(title="test video", description="test",
+                                          thumbnails=Thumbnails(default=Thumbnail("resources/profpic.jpg"))))
+        media = Media(filename="out/first_5_seconds.mp4")
+        upload = client.videos.insert(body=body, media=media, parts="snippet")
+        response = None
+        while response is None:
+            status, response = upload.next_chunk()
+            if status:
+                print(f"Upload {int(status.progress() * 100)} complete.")
+        print(f"Response body: {response}")
 
 
 if __name__ == "__main__":
